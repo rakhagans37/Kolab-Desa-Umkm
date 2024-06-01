21ba6ad5b7e0da1687acca80e682993e6e1b7614
Mzhssn <mzhssn313@gmail.com>
2024-06-01 13:57:42 +0700

    feat:initial commit branch staging

--- utils/footer.php
+++ utils/footer.php
@@ -4,7 +4,7 @@ if (!isset($footer)) {
 }
 
 echo '<footer class="site-footer">
-<img src="assets/images/icons/footer-bg-icon-1.png" class="site-footer__shape-1" alt="">
+
 <img src="assets/images/icons/footer-bg-icon-2.png" class="site-footer__shape-2" alt="">
 <div class="container">
     <div class="row" style="justify-content:space-between; margin: 0;">
